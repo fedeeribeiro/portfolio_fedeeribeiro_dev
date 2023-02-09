beb619a7d80efac8925a3030f26a762c9492093e
fedeeribeiro <ribeirofede@gmail.com>
2023-02-09 16:14:56 -0300

    site under construction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import './App.css';
+import "./App.css";
+import ConstructionIcon from "@mui/icons-material/Construction";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <ConstructionIcon />
+                <p>Site under construction.</p>
+            </header>
+        </div>
   );
 }
 
